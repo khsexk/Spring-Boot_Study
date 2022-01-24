b0c82c9ab00800f9a3153b6bad29a858b0b3da39
khsexk <kohyunsuk98@gmail.com>
2022-01-24 18:09:51 +0900

    3장 실습 진행

--- SPING QUICK START/Day_1/BoardWeb/src/main/java/polymorphism/TVUser.java
+++ SPING QUICK START/Day_1/BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -1,20 +1,28 @@
 package polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//interface를 이용하여 생
-		//TV tv = new SamsungTV();
+		/* 2장. interface를 이용하여 생성
+		TV tv = new SamsungTV(); 
+		2장. 팩토리 패턴 적용
+		BeanFactory factory = new BeanFactory();*/
 		
-		BeanFactory factory = new BeanFactory();
-		TV tv = (TV)factory.getBean(args[0]);
+		// 3장
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		TV tv = (TV)factory.getBean("tv"); 
 		
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
 		tv.powerOff();
+		
+		factory.close();
 	}
 
 }
