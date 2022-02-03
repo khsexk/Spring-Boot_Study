e9679c1fc5e92d7fce2363d2675af9e6a6e14afc
khsexk <kohyunsuk98@gmail.com>
2022-02-04 00:53:38 +0900

    Day2 - Class 05

--- SPING QUICK START/Day_2/BoardAOP/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ SPING QUICK START/Day_2/BoardAOP/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,9 +1,24 @@
 package com.springbook.biz.common;
 
+/*
 import org.aspectj.lang.JoinPoint;
 
 public class LogAdvice {
 	public void printLog(JoinPoint jp) {
+		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
+	}
+}
+*/
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Service;
+
+@Service
+@Aspect
+public class LogAdvice{
+	@Before("PointcutCommon.allPointcut")
+	public void printLog() {
 		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
 	}
 }
